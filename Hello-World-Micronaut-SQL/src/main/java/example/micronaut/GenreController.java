8283c36701f03eea494f4f7e1d0b43bc84f3ff26
Christopher D. Wade <wadec@objectcomputing.com>
2019-04-12 09:35:12 -0500

    added micronaut sql project

---
+++ Hello-World-Micronaut-SQL/src/main/java/example/micronaut/GenreController.java
@@ -0,0 +1,71 @@
+package example.micronaut;
+
+import example.micronaut.domain.Genre;
+import io.micronaut.http.HttpHeaders;
+import io.micronaut.http.HttpResponse;
+import io.micronaut.http.annotation.Body;
+import io.micronaut.http.annotation.Controller;
+import io.micronaut.http.annotation.Delete;
+import io.micronaut.http.annotation.Get;
+import io.micronaut.http.annotation.Post;
+import io.micronaut.http.annotation.Put;
+import io.micronaut.validation.Validated;
+
+import javax.validation.Valid;
+import java.net.URI;
+import java.util.List;
+
+@Validated // <1>
+@Controller("/genres") // <2>
+public class GenreController {
+
+    protected final GenreRepository genreRepository;
+
+    public GenreController(GenreRepository genreRepository) { // <3>
+        this.genreRepository = genreRepository;
+    }
+
+    @Get("/{id}") // <4>
+    public Genre show(Long id) {
+        return genreRepository
+                .findById(id)
+                .orElse(null); // <5>
+    }
+
+    @Put("/") // <6>
+    public HttpResponse update(@Body @Valid GenreUpdateCommand command) { // <7>
+        int numberOfEntitiesUpdated = genreRepository.update(command.getId(), command.getName());
+
+        return HttpResponse
+                .noContent()
+                .header(HttpHeaders.LOCATION, location(command.getId()).getPath()); // <8>
+    }
+
+    @Get(value = "/list{?args*}") // <9>
+    public List<Genre> list(@Valid SortingAndOrderArguments args) {
+        return genreRepository.findAll(args);
+    }
+
+    @Post("/") // <10>
+    public HttpResponse<Genre> save(@Body @Valid GenreSaveCommand cmd) {
+        Genre genre = genreRepository.save(cmd.getName());
+
+        return HttpResponse
+                .created(genre)
+                .headers(headers -> headers.location(location(genre.getId())));
+    }
+
+    @Delete("/{id}") // <11>
+    public HttpResponse delete(Long id) {
+        genreRepository.deleteById(id);
+        return HttpResponse.noContent();
+    }
+
+    protected URI location(Long id) {
+        return URI.create("/genres/" + id);
+    }
+
+    protected URI location(Genre genre) {
+        return location(genre.getId());
+    }
+}
